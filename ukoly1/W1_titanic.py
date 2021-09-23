b8062d7c5fe479839fdeec14d9f089580d98196f
hfa1978 <info@fakova.cz>
2021-09-23 19:40:17 +0200

    první úkol

---
+++ ukoly1/W1_titanic.py
@@ -0,0 +1,24 @@
+"""
+Titanic
+V souboru tested.csv najdeš informace o cestujících na zaoceánském parníku Titanic. Vytvoř kontingenční tabulku, která porovná závislost mezi pohlavím cestujícího (soupec Sex), třídou (sloupec Pclass), ve které cestoval, a tím, jesti přežil potopení Titanicu (sloupec Survived). Pro data můžeš použít agregaci numpy.sum, která ti spočte absolutní počet přeživších pro danou kombinaci, nebo numpy.mean, která udá relativní počet přeživších pro danou kombinaci.
+"""
+
+import pandas
+import requests
+import numpy
+
+r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/tested.csv")
+open("tested.csv", 'wb').write(r.content)
+
+cestujici = pandas.read_csv("tested.csv")
+#print(cestujici.columns)
+#print(cestujici.head())
+# Index(['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+#        'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
+#       dtype='object')
+
+cestujici_pivot1 = pandas.pivot_table(cestujici, values="Survived", index="Pclass", columns="Sex", aggfunc=numpy.sum, margins=True)
+print(cestujici_pivot1)
+
+cestujici_pivot2 = pandas.pivot_table(cestujici, values="Survived", index="Pclass", columns="Sex", aggfunc=numpy.mean, margins=True)
+print(cestujici_pivot2)
