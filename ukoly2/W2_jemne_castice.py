bf7034195b8973e350ca689ffe48fbc8198aac65
hfa1978 <info@fakova.cz>
2021-10-17 10:45:15 +0200

    Úkol 2 - změna funkce numpy.sum na numpy.mean

--- ukoly2/W2_jemne_castice.py
+++ ukoly2/W2_jemne_castice.py
@@ -25,7 +25,7 @@ print(air_polution.head())
 #print(air_polution.dtypes)
 
 """3. Vytvoř pivot tabulku s průměrným počtem množství jemných částic (sloupec pm25) v jednotlivých měsících a jednotlivých letech. Jako funkci pro agregaci můžeš použít numpy.mean."""
-air_polution_pivot_1 = pandas.pivot_table(air_polution, index="year", columns="month", values="pm25", aggfunc=numpy.sum, margins=False)
+air_polution_pivot_1 = pandas.pivot_table(air_polution, index="year", columns="month", values="pm25", aggfunc=numpy.mean, margins=False)
 print(air_polution_pivot_1)
 
 """Doplněk 1: Podívej se do první lekce na část o teplotních mapách a zobrat výsledek analýzy jako teplotní mapu."""
@@ -34,5 +34,5 @@ plt.show()
 
 """Doplněk 2: Použij metodu dt.dayofweek a přidej si do sloupce den v týdnu. Číslování je od 0, tj. pondělí má číslo 0 a neděle 6. Porovnej, jestli se průměrné množství jemných částic liší ve všední dny a o víkendu."""
 air_polution["dayofweek"] = air_polution["date"].dt.dayofweek
-air_polution_pivot_2 = pandas.pivot_table(air_polution, index="month", columns="dayofweek", values="pm25", aggfunc=numpy.sum, margins=True)
+air_polution_pivot_2 = pandas.pivot_table(air_polution, index="month", columns="dayofweek", values="pm25", aggfunc=numpy.mean, margins=True)
 print(air_polution_pivot_2)
